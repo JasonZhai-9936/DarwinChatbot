e2b93112c4e77c1487e8b0f090c0c8e04cae4d8b
JasonZhai-9936 <haotai@ualberta.ca>
2025-03-30 15:35:20 -0600

    Revert ".bat commit"
    This reverts commit 2ec446606f8396510b3c14d7a6ca1db9e5bc97d6.

--- git_push.bat
+++ git_push.bat
@@ -1,5 +1,5 @@
 @echo off
-<<<<<<< HEAD
+
 echo Adding changes (excluding /assets folder)...
 
 REM unstage anything in /assets just in case
@@ -8,21 +8,3 @@ git reset assets/
 REM 
 git add . ":!assets"
 
-echo Committing changes...
-git add .gitignore
-=======
-echo Adding changes...
-git add .
-
-echo Committing changes...
->>>>>>> 4fa721c10edb1576b1df781e65b0a795d2f89dd3
-git commit -m ".bat commit"
-
-echo Pushing to remote...
-git push
-
-<<<<<<< HEAD
-echo Finished.
-=======
-echo Finished
->>>>>>> 4fa721c10edb1576b1df781e65b0a795d2f89dd3
